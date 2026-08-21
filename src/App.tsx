f86a604030be2fc47cd669975f97c820199d790e
willyos0113 <willyos0113@gmail.com>
2026-08-21 13:52:28 +0800

    react state example

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import type { FC } from "react";
-import Greeting from './Greeting'
+import { useState, type FC } from "react";
 
 const App: FC = () => {
+  // 計數器
+  const [count, setCount] = useState<number>(0);
+
   return (
     <div className="App">
-      <h1>Hello, React!</h1>
-      <Greeting name="iThome 鐵人賽" />
+      <h1>React State Example</h1>
+      {/* 顯示 count 目前值 */}
+      <p>您點擊按鈕 {count} 次了</p>
+      {/* 點擊按鈕後，count 加 1 */}
+      <button onClick={() => setCount(count + 1)}>
+        點我
+      </button>
+      {/* 當 count 大於等於 10 次，顯示恭喜訊息 */}
+      {count >= 10 && <p>恭喜!!!您已點擊了 10 次！</p>}
     </div>
   )
 }
