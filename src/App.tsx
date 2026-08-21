0c4f6fb6aa6fae59419d450482411ae9e6bd14b5
willyos0113 <willyos0113@gmail.com>
2026-08-21 20:41:54 +0800

    add delete button with callback to trigger data removal in parent component

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,19 @@ const App: FC = () => {
   const [blogs, setBlogs] = useState([
     { title: "我們生來就是自由的", author: "艾倫", id: 1 },
     { title: "這個世界是很殘酷的，但是也非常美麗", author: "米卡莎", id: 2 },
-    { title: "什麼都無法捨棄的人，什麼都改變不了", author: "阿爾敏", id: 3 }
+    { title: "什麼都無法捨棄的人，什麼都改變不了", author: "阿爾敏", id: 3 },
   ]);
+  const handleDelete = (id: number) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
 
   return (
     <div className="App">
       <h1>我的部落格</h1>
-      <BlogList blogs={blogs} />
+      <BlogList blogs={blogs} onDelete={handleDelete} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/BlogList.tsx
+++ src/BlogList.tsx
@@ -1,25 +1,27 @@
 import { type FC } from "react";
 
 interface BlogItem {
-    title: string;
-    author: string;
-    id: number;
+  title: string;
+  author: string;
+  id: number;
 }
 interface BlogListProps {
-    blogs: BlogItem[];
+  blogs: BlogItem[];
+  onDelete: (id: number) => void;
 }
 
-const BlogList: FC<BlogListProps> = ({ blogs }) => {
-    return (
-        <div className="blog-list">
-            {blogs.map((blog) => (
-                <div key={blog.id} className="blog-item">
-                    <h3>{blog.title}</h3>
-                    <p>作者: {blog.author}</p>
-                </div>
-            ))}
+const BlogList: FC<BlogListProps> = ({ blogs, onDelete }) => {
+  return (
+    <div className="blog-list">
+      {blogs.map((blog) => (
+        <div key={blog.id} className="blog-item">
+          <h3>{blog.title}</h3>
+          <p>作者: {blog.author}</p>
+          <button onClick={() => onDelete(blog.id)}>刪除</button>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default BlogList;
