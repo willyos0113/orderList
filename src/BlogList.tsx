0b019e7619452606641b4bf8a6ed782e9e9b4e0d
willyos0113 <willyos0113@gmail.com>
2026-09-04 16:38:47 +0800

    Add a form (not done)

--- src/BlogList.tsx
+++ src/BlogList.tsx
@@ -1,8 +1,16 @@
-import { type FC } from "react";
+import { useState, type FC } from "react";
 import useFetch from "./useFetch";
 
 const BlogList: FC = () => {
   const { blogs, loading, error } = useFetch("http://localhost:8080/blogs");
+
+  const [title, setTitle] = useState<string>("");
+  const [author, setAuthor] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newBlog = { title, author };
+  };
 
   return (
     <div className="blog-list">
@@ -14,6 +22,20 @@ const BlogList: FC = () => {
           <p>{blog.author}</p>
         </div>
       ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="標題"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="作者"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+      </form>
     </div>
   );
 };
